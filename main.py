1e371637104c9a3cc9f23c75f8691bbd0bf6ae18
DanielmPC <leonmartinezdn@gmail.com>
2023-01-16 14:26:37 -0600

    Se actualizó main :rocket:

--- main.py
+++ main.py
@@ -1,23 +1,20 @@
 import cv2, time
 
-# Crear objeto video
 video = cv2.VideoCapture(0)
 
 while True: 
-
     
     check, frame = video.read()
     print(check)
     print(frame)    
 
-    # Mostramos el frame
     cv2.imshow('Capturing', frame)
 
     #cv2.waitKey(0)
 
     key = cv2.waitKey(1)
 
-    if key == ord('q'):      # Al presionar q la ventana se cieraa
+    if key == ord('q'):      # Al presionar q la ventana se ciera
         break 
 
 
